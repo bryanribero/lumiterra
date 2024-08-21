ef2c11ac461bae9d85e34c4f282f7c473d835118
Bryan <bryanribero@hotmail.com>
2024-08-21 06:52:56 -0300

    ajustando videos

--- js/menu.js
+++ js/menu.js
@@ -6,25 +6,12 @@ window.addEventListener('scroll', function(){
 
 const videos = document.querySelectorAll('.video1');
 
-// Itera sobre cada video y añade los eventos correspondientes
 videos.forEach(video => {
     video.addEventListener('loadedmetadata', function() {
         video.currentTime = 1;  // Configura el video para que empiece en el primer segundo
-        video.pause();  // Pausa el video para que no siga reproduciéndose
+        video.play();  // Reproduce el video automáticamente
     });
 
-    video.addEventListener('mouseover', function() {
-        video.play();
-    });
-
-    video.addEventListener('mouseout', function() {
-        video.pause();
-        video.currentTime = 1;  // Regresa al primer segundo del video
-    });
+    video.loop = true; // Configura el video para que se reproduzca en bucle
 });
 
-window.addEventListener('load', function() {
-    const video = document.getElementsByClassName('video1');
-    video.pause(); // Asegúrate de que el video esté en pausa al cargar
-    video.currentTime = 0; // Asegura que el video esté al inicio para mostrar el póster
-});
